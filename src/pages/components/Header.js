0821e81279fa4f5479c6ccae613de09479661584
Owlpetcrypto <owlpetcrypto@gmail.com>
2023-03-22 00:44:20 +0100

    'Logo'

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,10 +1,19 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Plane from '../../styles/paperplane.svg'
 
 function Header() {
   return (
     <header>
       <h1>Together.</h1>
+      <Image
+        className="header__plane"
+        src={Plane}
+        alt="Picture of the author"
+        width={150}
+        height={100}
+      />
       <ul>
         <Link href="#" replace>
           Home
@@ -18,7 +27,7 @@ function Header() {
         <Link href="login">Login</Link>
       </ul>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
